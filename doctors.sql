c68bbb8a7be3484a2453947505ceac4aba65e5aa
Ineza123-art <inezajoy123@gmail.com>
2026-02-05 14:12:45 +0200

    Update doctors.sql

--- doctors.sql
+++ doctors.sql
@@ -1,1 +1,2 @@
 create database medicalsystem;
+create table doctors(doc_id INT PRIMARY KEY AUTO_INCREMENT,f_name VARCHAR(50) NOT NULL,l_name VARCHAR(50) NOT NULL,specialization VARCHAR(100) NOT NULL,department VARCHAR(50) NOT NULL,salary INT NOT NULL);
